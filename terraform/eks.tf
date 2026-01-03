934c5bf781201d8cd6fd897afffb36ccec92ff97
KhaushikParvatham <kaushikp2002@gmail.com>
2026-01-03 10:52:03 +0530

    feat: add eks cluster and managed node group

---
+++ terraform/eks.tf
@@ -0,0 +1,26 @@
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 20.0"
+
+  cluster_name    = var.project_name
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access = true
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_groups = {
+    default = {
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
+
+  tags = {
+    Project = var.project_name
+  }
+}
